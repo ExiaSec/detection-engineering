a5af1a5e85e895a48f16078e4755cda898c04451
Gregory Dandoy <epyon6415@gmail.com>
2025-03-25 14:05:58 +0100

    adding failure statements

--- development/validation.py
+++ development/validation.py
@@ -2,12 +2,14 @@ import tomllib
 import sys
 import os
 
-#file = "alert_example.toml"
+# file = "alert_example.toml"
 # with open(file,"rb") as toml:
-#    alert = tomllib.load(toml)
+#     alert = tomllib.load(toml)
 
-for root, dirs, files in os.walk ("D:\\Detection Engineering for Beginners\\Scripts\\GITHUB\\converted_detections"):
-    for file in files: 
+failure = 0
+
+for root, dirs, files in os.walk("detections/"):
+    for file in files:
         if file.endswith(".toml"):
             full_path = os.path.join(root, file)
             with open(full_path,"rb") as toml:
@@ -16,16 +18,23 @@ for root, dirs, files in os.walk ("D:\\Detection Engineering for Beginners\\Scri
                 present_fields = []
                 missing_fields = []
 
+                try:
+                    if alert['metadata']['creation_date']:
+                       pass 
+                except:
+                    print("The metadata table does not contain a creation_date on: " + full_path)
+                    failure = 1
+                    
+
                 if alert['rule']['type'] == "query": # query based alert
-                    required_fields = ['description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query']
+                    required_fields = ['description', 'name','rule_id','risk_score','severity','type','query']
                 elif alert['rule']['type'] == "eql": # event correlation alert
-                    required_fields = ['description', 'name', 'rule_id','risk_score', 'severity', 'type', 'query', 'language']
+                    required_fields = ['description', 'name','rule_id','risk_score','severity','type','query','language']
                 elif alert['rule']['type'] == "threshold": # threshold based alert
-                    required_fields = ['description', 'name', 'rule_id', 'risk_score', 'severity', 'type', 'query', 'threshold']
+                    required_fields = ['description', 'name','rule_id','risk_score','severity','type','query','threshold']
                 else:
                     print("Unsupported rule type found in: " + full_path)
                     break
-
                 for table in alert:
                     for field in alert[table]:
                         present_fields.append(field)
@@ -36,5 +45,9 @@ for root, dirs, files in os.walk ("D:\\Detection Engineering for Beginners\\Scri
 
                 if missing_fields:
                     print("The following fields do not exist in " + file + ": " + str(missing_fields))
+                    failure = 1
                 else:
-                    print("validation Passed for: " + file)
+                    print("Validation Passed for: " + file)
+
+if failure != 0:
+    sys.exit(1)
